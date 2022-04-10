c26dff7a07179164f0a23dff49568a699cf70ca4
ShahzodXjon <68816662+ShaxaX@users.noreply.github.com>
2022-04-10 12:47:38 +0500

    Update settings.py

--- Youtube-Search-and-Player-master/Youtube_Search/settings.py
+++ Youtube-Search-and-Player-master/Youtube_Search/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'eujv#a8mj8dkt=ls$1j*-e(c*=o#52+vw#@v6p@@j-z7=b$c@2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','youtube-t-w.herokuapp.com']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
